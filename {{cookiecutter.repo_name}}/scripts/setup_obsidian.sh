c69d9d8b20e131c1ee75108a92cc41034d1d22bb
Pedro <pedro@Mac.hsd1.wa.comcast.net>
2025-12-21 21:14:43 -0800

    typo fixed

--- {{cookiecutter.repo_name}}/scripts/setup_obsidian.sh
+++ {{cookiecutter.repo_name}}/scripts/setup_obsidian.sh
@@ -1,22 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
-# Save where the user was
 ORIGINAL_DIR="$(pwd)"
-
-# Resolve the directory this script lives in (even if called via symlink)
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
-
-# Move to script directory
 cd "$SCRIPT_DIR"
-# --- script logic here ---
-project_folder_name=~/Vaults/06-Project_Notes/{{ cookiecutter.repo_name }}
 
-mkdir {{ cookiecutter.repo_name }}
+repo_name="{{ cookiecutter.repo_name }}"
 
-ln -s ~/Projects/{{cookiecutter.repo_name}}/Progress_Log.md ${{project_folder_name}}/Progress_Log.md
-ln -s ~/Projects/{{cookiecutter.repo_name}}/figures/ ${{project_folder_name}}/figures
+project_folder_name="$HOME/Vaults/06-Project_Notes/$repo_name"
 
-# Return to where the user was
+mkdir -p "$project_folder_name"
+
+ln -sf "$HOME/Projects/$repo_name/Progress_Log.md" "$project_folder_name/Progress_Log.md"
+ln -sfn "$HOME/Projects/$repo_name/figures"         "$project_folder_name/figures"
+
 cd "$ORIGINAL_DIR"
-
